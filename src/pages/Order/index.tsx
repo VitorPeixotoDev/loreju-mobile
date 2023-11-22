890d4e87fdce56d0bd803ad21163ce73325d13b3
vitor2204 <vitorpeixotoc137@gmail.com>
2023-11-21 21:10:55 -0300

    receiving params

--- src/pages/Order/index.tsx
+++ src/pages/Order/index.tsx
@@ -1,9 +1,23 @@
 import { View, Text, StyleSheet } from 'react-native'
+import { useRoute, RouteProp } from '@react-navigation/native'
+
+type RouterDetaiParams = {
+    Order: {
+        number: number | string
+        order_id: string
+    }
+}
+
+type OrderRouteProps = RouteProp<RouterDetaiParams, 'Order'>
 
 const Order = () => {
+    const route = useRoute<OrderRouteProps>()
+
     return(
         <View style={styles.container}>
             <Text>Tela de Order</Text>
+            <Text>{route.params.number}</Text>
+            <Text>{route.params.order_id}</Text>
         </View>
     )
 }
